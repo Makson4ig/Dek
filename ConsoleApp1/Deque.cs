28deda53df6ceb8163a77fc7ce751c18e333e133
User <User@MAXIM-PC>
2019-04-18 16:23:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Deque.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Deque<T> : IEnumerable<T>  // двусвязный список
+    {
+        DoublyNode<T> head; // головной/первый элемент
+        DoublyNode<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        // добавление элемента в конец
+        public void AddLast(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+        // добавление элемента в начало
+        public void AddFirst(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+            DoublyNode<T> temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+        // удаление элемента в начале
+        public T RemoveFirst()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = head.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.Next;
+                head.Previous = null;
+            }
+            count--;
+            return output;
+        }
+        // удаление элемента в конце
+        public T RemoveLast()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = tail.Data;
+            if (count == 1)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                tail = tail.Previous;
+                tail.Next = null;
+            }
+            count--;
+            return output;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deque<string> usersDeck = new Deque<string>();
+            string s = Console.ReadLine();
+            //EAs+Y+QUE**+st+*+IO*n++*
+            foreach (char c in s)
+            {
+                if (Regex.IsMatch(c.ToString(), @"[A-Z]"))
+                {
+                    usersDeck.AddFirst(c.ToString());
+                }
+                else if (Regex.IsMatch(c.ToString(), @"[a-z]"))
+                {
+                    usersDeck.AddLast(c.ToString()); 
+                }
+                else if (c.ToString() == "+")
+                {
+                    usersDeck.RemoveFirst();
+                }
+                else if (c.ToString() == "*")
+                {
+                    usersDeck.RemoveLast();
+                }
+            }
+
+            foreach (string i in usersDeck)
+                Console.WriteLine(i);
+            Console.WriteLine("Выполнено!");
+            Console.ReadKey();
+        }
+    }
+
+    public class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+}
